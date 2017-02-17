51d97e3c2b76f3384451cc7be18eafe0d00bfc7d
Szymon Koszarek <szymkosz@icloud.com>
2017-02-17 13:42:17 -0600

    Read data into program

--- Case Study 1/CaseStudyOneB.py
+++ Case Study 1/CaseStudyOneB.py
@@ -1,2 +1,12 @@
 import csv
 import matplotlib.pyplot as plt
+
+fileName = "FlightDelay.csv"
+
+'''Read Flight Delay data into program'''
+flightDelayData = []
+with open(fileName, 'r') as file:
+    myReader = csv.reader(file):
+    next(myReader)
+    for line in myReader:
+        flightDelayData.append(line)
