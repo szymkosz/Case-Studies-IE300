fa7fa7d01b48f355586e1cfa96e6e7ae17597316
Szymon Koszarek <szymkosz@icloud.com>
2017-02-17 16:13:54 -0600

    Isolate airline, origin, destination into list

--- Case Study 1/CaseStudyOneB.py
+++ Case Study 1/CaseStudyOneB.py
@@ -4,16 +4,31 @@ import matplotlib.pyplot as plt
 fileName = "FlightDelay.csv"
 
 '''Read Flight Delay data into program'''
-flightDelayData = []
+rawFlightDelayData = []
 with open(fileName, 'r') as file:
     myReader = csv.reader(file)
     next(myReader)
     for line in myReader:
-        flightDelayData.append(line)
+        rawFlightDelayData.append(line)
 
-for i in flightDelayData:
+### Classify each flight as Delayed/Not Delayed (Y/N)
+for i in rawFlightDelayData:
     if (int(i[3]) + int(i[4]) > 15):
         i.append("Y")
     else:
         i.append("N")
-    print(i)
+
+### Isolate each airline, origin, and destination into a list
+airlines = []
+origins = []
+destinations = []
+
+for element in rawFlightDelayData:
+    if element[0] not in airlines:
+        airlines.append(element[0])
+    if element[1] not in origins:
+        origins.append(element[1])
+    if element[2] not in destinations:
+        destinations.append(element[2])
+
+### Separate each attribute per airline
