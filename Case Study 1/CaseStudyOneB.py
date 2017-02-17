36035fb220a80c09f359d0efa80e1fac4dfdb557
Szymon Koszarek <szymkosz@icloud.com>
2017-02-17 13:48:42 -0600

    Appended Y/N to show delay status

--- Case Study 1/CaseStudyOneB.py
+++ Case Study 1/CaseStudyOneB.py
@@ -6,7 +6,14 @@ fileName = "FlightDelay.csv"
 '''Read Flight Delay data into program'''
 flightDelayData = []
 with open(fileName, 'r') as file:
-    myReader = csv.reader(file):
+    myReader = csv.reader(file)
     next(myReader)
     for line in myReader:
         flightDelayData.append(line)
+
+for i in flightDelayData:
+    if (int(i[3]) + int(i[4]) > 15):
+        i.append("Y")
+    else:
+        i.append("N")
+    print(i)
