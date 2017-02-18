0637fcb1d8dd4be5564766a73fad1bd9828a9d03
Szymon Koszarek <szymkosz@icloud.com>
2017-02-17 21:52:26 -0600

    Initialize various variable for probability calcs

--- Case Study 1/CaseStudyOneB.py
+++ Case Study 1/CaseStudyOneB.py
@@ -2,6 +2,7 @@ import csv
 import matplotlib.pyplot as plt
 
 fileName = "FlightDelay.csv"
+m = 3           # m-value for M-estimates
 
 '''Read Flight Delay data into program'''
 rawFlightDelayData = []
@@ -11,14 +12,14 @@ with open(fileName, 'r') as file:
     for line in myReader:
         rawFlightDelayData.append(line)
 
-### Classify each flight as Delayed/Not Delayed (Y/N)
+''' Classify each flight as Delayed/Not Delayed (Y/N) '''
 for i in rawFlightDelayData:
     if (int(i[3]) + int(i[4]) > 15):
         i.append("Y")
     else:
         i.append("N")
 
-### Isolate each airline, origin, and destination into a list
+''' Isolate each airline, origin, and destination into a list '''
 airlines = []
 origins = []
 destinations = []
@@ -31,7 +32,7 @@ for element in rawFlightDelayData:
     if element[2] not in destinations:
         destinations.append(element[2])
 
-### Separate each attribute per airline
+''' Separate each attribute per airline '''
 airlineFlightDelay = {k: {} for k in airlines}
 for key in airlineFlightDelay.keys():
     for origin in origins:
@@ -43,6 +44,34 @@ for key in airlineFlightDelay.keys():
         airlineFlightDelay[key][dest]["Y"] = 0
         airlineFlightDelay[key][dest]["N"] = 0
 
+''' Increment Y/N attribute per airport per airline '''
+numDelay = 0
+numNoDelay = 0
+
 for i in rawFlightDelayData:
     airlineFlightDelay[i[0]][i[1]][i[5]] += 1
     airlineFlightDelay[i[0]][i[2]][i[5]] += 1
+
+  ## Count Total Delay/No Delay
+    if i[5] == "Y":
+        numDelay += 1
+    elif i[5] == "N":
+        numNoDelay += 1
+
+### Pre-calculate probabilities
+P_ORIGIN = 1/len(origins)
+P_DESTINATION = 1/len(destinations)
+P_CARRIER = 1/len(airlines)
+numOfObsv = len(rawFlightDelayData)
+
+probY = numDelay/numOfObsv
+probN = numNoDelay/numOfObsv
+
+
+# Neatly prints all data per airline
+'''
+for key in airlineFlightDelay.keys():
+    print(key)
+    for i in airlineFlightDelay[key]:
+        print(i, airlineFlightDelay[key][i])
+'''
